1ab1b94c7f7abac357083e78b9fd64c9d1deb054
nms377 <nms377@nyu.edu>
2016-12-05 21:09:47 -1000

    Add rainbow.

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -167,6 +167,19 @@ browsers = {
  *
  */
 
+rainbow = { 
+	colors:{
+		red: '#F0280A',
+		orange: '#FF8800',
+		yellow: '#FFDD00',
+		green: '#51AB0C',
+		blue: '#1593ED',
+		indigo: '#5215ED',
+		violet: '#A915ED',
+	},
+
+	isDouble: true
+};
 
 /* Step 14
  *
