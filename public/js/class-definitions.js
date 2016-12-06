913f9e6681c6fa0dd2d0f6b663f521210528a329
nms377 <nms377@nyu.edu>
2016-12-05 21:02:58 -1000

    Was a little stumped by domains. Realized it would pass if I turned the properties and values into string values.

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -120,6 +120,12 @@ princess_leia = {
  *
  */
 
+domains = {
+	'ycombinator.com': '198.41.190.47',
+	'laughingsquid.com': '162.159.247.97',
+	'slumlordhosting.com': '198.61.179.126',
+	'jsonformatter.curiousconcept.com': '104.28.5.70'
+};
 
 /* Step 12
  *
