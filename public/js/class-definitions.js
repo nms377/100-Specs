f2c51e323cdc8ceae8c8085fd7cf511f4a3090ff
nms377 <nms377@nyu.edu>
2016-12-06 22:56:12 -1000

    Step 77

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1186,7 +1186,7 @@ var halogen = new Lightbulb(false);
 
 // Create 2 cookies of different flavors
 var chocolateChip = new Cookie('chocolate');
-var gingerbread;
+var gingerbread = new Cookie('gingerbread');
 
 // Create 2 different meals
 var breakfast;
