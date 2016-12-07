755185e6c6ca9955e054f4a6fe05121d7db95d66
nms377 <nms377@nyu.edu>
2016-12-06 23:01:00 -1000

    Step 79

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1190,7 +1190,7 @@ var gingerbread = new Cookie('gingerbread');
 
 // Create 2 different meals
 var breakfast = new Meal(['cereal', 'milk']);
-var dinner;
+var dinner = new Meal(['fish', 'vegetables']);
 
 
  /* Steps 81 to 90
