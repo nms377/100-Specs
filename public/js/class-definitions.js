94086b1d9203d4521817cb9bb7f48ead5b52a4df
nms377 <nms377@nyu.edu>
2016-12-06 23:21:51 -1000

    Step 85

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1309,6 +1309,15 @@ Box.prototype.openBox = function(){
  *
  */
 
+Door.prototype.openClose = function(){
+	if(this.isOpen === false){
+		this.isOpen = true;
+		return true;
+	}else if(this.isOpen === true){
+		this.isOpen = false;
+		return false;
+	}
+};
 
 /* Step 86
  *
