934650620adca090d77b4db908588a13e1f7230b
nms377 <nms377@nyu.edu>
2016-12-06 23:43:35 -1000

    Step 95

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1462,8 +1462,8 @@ var closeAnOpenBox = new Box('foods', true).openBox();
  * and assign the values to each variable below.
  *
  */
-var openAClosedDoor;
-var closeAnOpenDoor;
+var openAClosedDoor = new Door(false).openClose();
+var closeAnOpenDoor = new Door(true).openClose();
 
 
 /* Step 96
