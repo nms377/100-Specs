2d0988ab2eb984fc79d29c745ef7d65a758a090f
nms377 <nms377@nyu.edu>
2016-12-06 23:26:43 -1000

    Step 89

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1375,6 +1375,13 @@ Lightbulb.prototype.flipSwitch = function(on){
  *
  */
 
+Cookie.prototype.swipedByCookieMonster = function(dayOfTheWeek){
+	if(this.flavor === 'chocolate' && dayOfTheWeek === 'Monday'){
+		return true;
+	}else{
+		return false;
+	}
+}
 
  /* Step 90
  *
